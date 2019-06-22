de315bce7ea344523c7b06ac1d5f1eebeae9127c
pamela-qnt <pamela.quint.ifc@gmail.com>
2019-06-22 16:17:50 -0300

    Banco de Dados
    Atualizei o banco de dados

--- Modelos/3 - Fisico/FISICO_PETSHOPE.sql
+++ Modelos/3 - Fisico/FISICO_PETSHOPE.sql
@@ -4,53 +4,67 @@
 
 
 CREATE TABLE nf_compra (
-data_compra VARCHAR(50),
-id_usuario VARCHAR(50),
-hora_compra VARCHAR(50),
-valor_compra VARCHAR(50)
-)
+data_compra date,
+id_usuario int(11),
+hora_compra time,
+valor_compra decimal(10,2),
+FOREIGN KEY(id_usuario) REFERENCES usuario (id_usuario)
+);
 
 CREATE TABLE produto (
-id_produto VARCHAR(50) PRIMARY KEY,
-qtd_estoque VARCHAR(50),
-descricao VARCHAR(50),
-valor VARCHAR(50),
-id_classificacao VARCHAR(50)
-)
+id_produto int(11) PRIMARY KEY AUTO_INCREMENT,
+qtd_estoque int(11),
+desc_produto varchar(300),
+valor decimal(10,2),
+desc_tipo_prod varchar(50),
+FOREIGN KEY (desc_tipo_prod) REFERENCES tipo_produto (id_tipo_prod)
+);
 
 CREATE TABLE animal (
-nome_animal VARCHAR(50),
-id_animal VARCHAR(50) PRIMARY KEY,
-sexo VARCHAR(50)
-)
+nome_animal varchar(50),
+id_animal int(11) PRIMARY KEY AUTO_INCREMENT,
+sexo char(1)
+);
 
-CREATE TABLE especie (
-id_animal VARCHAR(50),
-especie VARCHAR(50),
-descricao VARCHAR(50),
+CREATE TABLE carrinho (
+id_pedido int(11) PRIMARY KEY AUTO_INCREMENT, 
+id_usuario int(11), 
+data_pedido date, 
+total_compra decimal(10,2),
+FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
+);
+
+
+CREATE TABLE especie_animal (
+id_animal int(11),
+desc_especie varchar(300),
 FOREIGN KEY(id_animal) REFERENCES animal (id_animal)
-)
+);
 
 CREATE TABLE usuario (
-id_usuario VARCHAR(50) PRIMARY KEY,
-nome_usuario VARCHAR(50),
-email VARCHAR(50),
-tel_contato VARCHAR(50),
-senha VARCHAR(50),
-id_animal VARCHAR(50),
-FOREIGN KEY(id_animal) REFERENCES animal (id_animal)
-)
+id_usuario int(11) PRIMARY KEY AUTO_INCREMENT,
+nome_usuario varchar(100),
+email varchar(100),
+tel_contato int(11),
+senha varchar(50),
+desc_tipo_usuario varchar(50),
+FOREIGN KEY(desc_tipo_usuario varchar) REFERENCES tipo_usuario (id_tipo_usuario)
+);
 
-CREATE TABLE classificacao (
-id_classificacao VARCHAR(50) PRIMARY KEY,
-tipo_prod VARCHAR(50),
-id_produto VARCHAR(50),
+CREATE TABLE tipo_produto (
+id_tipo_prod int(11) PRIMARY KEY AUTO_INCREMENT,
+desc_tipo_prod varchar(50),
+);
+
+CREATE TABLE tipo_usuario (
+id_tipo_usuario int(11), 
+desc_tipo_usuario varchar(50)
+);
+
+
+//CREATE TABLE compra (
+id_usuario int(11),
+id_produto int(11),
+FOREIGN KEY(id_usuario) REFERENCES usuario (id_usuario),
 FOREIGN KEY(id_produto) REFERENCES produto (id_produto)
-)
-
-CREATE TABLE compra (
-id_usuario VARCHAR(50),
-id_produto VARCHAR(50),
-PRIMARY KEY(id_usuario,id_produto)
-)
-
+);
